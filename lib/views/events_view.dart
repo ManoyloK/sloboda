5c2f0c61a71d3efb38a974e940bd6490f2f50ed3
Dmytro Gladkyi <gladimdim@gmail.com>
2020-03-28 19:01:06 +0200

    EventsView displays either incoming or passed events.
    No more 50% split between two lists.

--- lib/views/events_view.dart
+++ lib/views/events_view.dart
@@ -49,90 +49,95 @@ class EventsView extends StatelessWidget {
               events: pendingEvents,
             ),
           ),
-        Expanded(
-          flex: 2,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SoftContainer(
-              child: ListView.separated(
-                itemCount: _events.keys.length,
-                separatorBuilder: (context, index) {
-                  return VDivider();
-                },
-                itemBuilder: (context, index) {
-                  final keysList = _events.keys.toList();
-                  keysList.sort((a, b) {
-                    var year1 = a.split(',')[0];
-                    var year2 = b.split(',')[0];
+        if (pendingEvents.isEmpty)
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SoftContainer(
+                child: ListView.separated(
+                  itemCount: _events.keys.length,
+                  separatorBuilder: (context, index) {
+                    return VDivider();
+                  },
+                  itemBuilder: (context, index) {
+                    final keysList = _events.keys.toList();
+                    keysList.sort((a, b) {
+                      var year1 = a.split(',')[0];
+                      var year2 = b.split(',')[0];
 
-                    return year1.compareTo(year2);
-                  });
-                  final key = keysList[keysList.length - 1 - index];
-                  var seasonKey = key.split(',')[1];
-                  var year = key.split(',')[0];
-                  return SoftContainer(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          SoftContainer(
-                            child: Container(
-                              height: 40,
-                              child: FullWidth(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TitleText(
-                                      '${SlobodaLocalizations.getForKey(seasonKey)} ${year.toString()}'),
+                      return year1.compareTo(year2);
+                    });
+                    final key = keysList[keysList.length - 1 - index];
+                    var seasonKey = key.split(',')[1];
+                    var year = key.split(',')[0];
+                    return SoftContainer(
+                      child: Padding(
+                        padding: const EdgeInsets.all(.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            SoftContainer(
+                              child: Container(
+                                height: 40,
+                                child: FullWidth(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TitleText(
+                                        '${SlobodaLocalizations.getForKey(seasonKey)} ${year.toString()}'),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          ..._events[key].reversed.map((event) {
-                            var textStyle;
-                            if (city.currentSeason.isNextTo(event.season)) {
-                              textStyle = Theme.of(context).textTheme.headline6;
-                            }
-                            return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: SoftContainer(
-                                  child: Column(
-                                    children: <Widget>[
-                                      if (event.sourceEvent.imagePath != null)
-                                        Image.asset(
-                                          event.sourceEvent.imagePath,
-                                          width: 64,
+                            ..._events[key].reversed.map((event) {
+                              var textStyle;
+                              if (city.currentSeason.isNextTo(event.season)) {
+                                textStyle =
+                                    Theme.of(context).textTheme.headline6;
+                              }
+                              return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SoftContainer(
+                                    child: Column(
+                                      children: <Widget>[
+                                        if (event.sourceEvent.imagePath != null)
+                                          Image.asset(
+                                            event.sourceEvent.imagePath,
+                                            width: 64,
+                                          ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: FullWidth(
+                                            child: Text(
+                                              SlobodaLocalizations.getForKey(
+                                                  event.sourceEvent.messageKey),
+                                              textAlign: TextAlign.center,
+                                              style: textStyle,
+                                            ),
+                                          ),
                                         ),
-                                      FullWidth(
-                                        child: Text(
-                                          SlobodaLocalizations.getForKey(
-                                              event.sourceEvent.messageKey),
-                                          textAlign: TextAlign.center,
-                                          style: textStyle,
-                                        ),
-                                      ),
-                                      if (event.sourceEvent.stock != null)
-                                        StockMiniView(
-                                          stock: event.sourceEvent.stock,
-                                          stockSimulation: null,
-                                        ),
-                                      if (event.sourceEvent.cityProps != null)
-                                        CityPropsMiniView(
-                                          props: event.sourceEvent.cityProps,
-                                        ),
-                                    ],
-                                  ),
-                                ));
-                          }).toList(),
-                        ],
+                                        if (event.sourceEvent.stock != null)
+                                          StockMiniView(
+                                            stock: event.sourceEvent.stock,
+                                            stockSimulation: null,
+                                          ),
+                                        if (event.sourceEvent.cityProps != null)
+                                          CityPropsMiniView(
+                                            props: event.sourceEvent.cityProps,
+                                          ),
+                                      ],
+                                    ),
+                                  ));
+                            }).toList(),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
           ),
-        ),
       ],
     );
   }
